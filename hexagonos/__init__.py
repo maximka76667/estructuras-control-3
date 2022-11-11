1aa3ea620e7f976ffbfe5637a81cf1cbb2cfaf9a
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-11-11 17:11:03 +0100

    hexagons

--- hexagonos/__init__.py
+++ hexagonos/__init__.py
@@ -1,8 +1,10 @@
 def printHexagon(n, c):
     for i in range(1, 2 * n, 1):
-        print(abs(i - n) * " " + c * int((n - abs(n - i) - 1) * 2 + n))
+        # Cantidad de espacios en linea = abs(i - n)
+        # Cantidad de symbols en linea = int((n - abs(n - i) - 1) * 2 + n)
+        print(abs(i - n) * " " + int((n - abs(n - i) - 1) * 2 + n) * c)
 
-input = input("Hexagon options: ")
+input = input("Hexagon options (n c): ")
 [n,_,c] = input
         
 printHexagon(int(n), c)
